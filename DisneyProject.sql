77cb51c278704d6a9ccebc2199e5d8eb2c64fa7d
lisad98 <134489150+lisad98@users.noreply.github.com>
2023-05-27 06:03:42 -0500

    Add files via upload

---
+++ DisneyProject.sql
@@ -0,0 +1,30 @@
+SELECT *
+FROM PortfolioProject.dbo.disney$
+ORDER BY release_date, movie_title
+
+--genres with highest total gross
+
+SELECT genre, MAX(total_gross) AS genre_total_gross
+FROM PortfolioProject.dbo.disney$
+GROUP BY genre
+ORDER BY genre_total_gross
+
+--release date vs inflation adjusted gross
+
+SELECT release_date, inflation_adjusted_gross
+FROM PortfolioProject.dbo.disney$
+ORDER BY release_date
+
+--mpaa_rating with highest total gross
+
+SELECT mpaa_rating, MAX(total_gross) AS mpaa_total_gross
+FROM PortfolioProject.dbo.disney$
+GROUP BY mpaa_rating
+ORDER BY mpaa_total_gross
+
+--total gross vs inflation adjusted gross
+
+SELECT movie_title, total_gross, inflation_adjusted_gross
+FROM PortfolioProject.dbo.disney$
+ORDER BY movie_title
+
